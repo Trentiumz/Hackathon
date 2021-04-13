53d1b3cc996ecd382785860c8e7878a4c585a93f
Daniel Ye <danielyeqt@gmail.com>
2021-04-13 19:23:45 -0400

    Initial Update

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var canvas;
+var context;
+
+
 window.onload = function(){
   // Starting code of the program
   console.log("starting");
+
+  canvas = document.getElementById("gameCanvas");
+  context = canvas.getContext("2d");
 }
